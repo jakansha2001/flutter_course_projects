70b070ed3a216f2e53e5720419fe90d929021162
jakansha2001 <jakansha2001@gmail.com>
2022-11-01 00:50:46 +0530

    new project initialised

---
+++ dicee_flutter/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Dicee',
+            ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Dicee Flutter App',
+    );
+  }
+}

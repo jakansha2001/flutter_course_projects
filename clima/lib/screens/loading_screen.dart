6301b2d61e932a68930fdc7f5be221aa4b62e526
jakansha2001 <jakansha2001@gmail.com>
2023-01-05 03:45:10 +0530

    Initialised Clima
    This is a new flutter project

---
+++ clima/lib/screens/loading_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}

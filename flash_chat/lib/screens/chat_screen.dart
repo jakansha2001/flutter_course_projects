f63ed12a2a85169d53fd03e6bef4541a3f1ff453
jakansha2001 <jakansha2001@gmail.com>
2023-01-10 04:01:14 +0530

    implemented logout functionality in Chat Screen

--- flash_chat/lib/screens/chat_screen.dart
+++ flash_chat/lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: library_private_types_in_public_api
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -22,7 +23,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: const Icon(Icons.close),
               onPressed: () {
-                //Implement logout functionality
+                FirebaseAuth.instance.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: const Text('⚡️Chat'),

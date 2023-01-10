a357065ded4e4d202fc046726601ac785c7a09d5
jakansha2001 <jakansha2001@gmail.com>
2023-01-10 18:13:03 +0530

    Added Spinner

--- flash_chat/lib/screens/login_screen.dart
+++ flash_chat/lib/screens/login_screen.dart
@@ -19,97 +19,106 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   String? userEmail;
   String? password;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: SizedBox(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
-                ),
-              ),
-              const SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  userEmail = value;
-                },
-                decoration: textFieldInputDecoration.copyWith(
-                  hintText: 'Enter your email',
-                ),
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                obscureText: true,
-                keyboardType: TextInputType.visiblePassword,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: textFieldInputDecoration.copyWith(
-                  hintText: 'Enter your password',
-                ),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              RoundedButton(
-                buttonText: 'Log In',
-                color: Colors.lightBlueAccent,
-                onPressed: () async {
-                  FocusManager.instance.primaryFocus!.unfocus();
-                  if (userEmail == null && password == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('E-mail Id and Password, both can\'t be empty!!'),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Hero(
+                      tag: 'logo',
+                      child: SizedBox(
+                        height: 200.0,
+                        child: Image.asset('images/logo.png'),
                       ),
-                    );
-                  } else if (userEmail == null || password == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('E-mail Id or Password can\'t be empty!!'),
+                    ),
+                    const SizedBox(
+                      height: 48.0,
+                    ),
+                    TextField(
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (value) {
+                        userEmail = value;
+                      },
+                      decoration: textFieldInputDecoration.copyWith(
+                        hintText: 'Enter your email',
                       ),
-                    );
-                  } else {
-                    final message = await AuthService.login(email: userEmail!, password: password!);
-                    if (message.contains('Success')) {
-                      Navigator.pushNamed(context, ChatScreen.id);
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(message),
-                        ),
-                      );
-                    }
-                  }
-                },
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    TextField(
+                      textAlign: TextAlign.center,
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      decoration: textFieldInputDecoration.copyWith(
+                        hintText: 'Enter your password',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    RoundedButton(
+                      buttonText: 'Log In',
+                      color: Colors.lightBlueAccent,
+                      onPressed: () async {
+                        FocusManager.instance.primaryFocus!.unfocus();
+                        if (userEmail == null && password == null) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('E-mail Id and Password, both can\'t be empty!!'),
+                            ),
+                          );
+                        } else if (userEmail == null || password == null) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('E-mail Id or Password can\'t be empty!!'),
+                            ),
+                          );
+                        } else {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          final message = await AuthService.login(email: userEmail!, password: password!);
+                          setState(() {
+                            isLoading = false;
+                          });
+                          if (message.contains('Success')) {
+                            Navigator.pushNamed(context, ChatScreen.id);
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(message),
+                              ),
+                            );
+                          }
+                        }
+                      },
+                    ),
+                    RoundedButton(
+                      buttonText: 'Register',
+                      color: Colors.grey.shade400,
+                      onPressed: () async {
+                        Navigator.pushNamed(context, RegistrationScreen.id);
+                      },
+                    ),
+                  ],
+                ),
               ),
-              RoundedButton(
-                buttonText: 'Register',
-                color: Colors.grey.shade400,
-                onPressed: () async {
-                  Navigator.pushNamed(context, RegistrationScreen.id);
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
--- flash_chat/lib/screens/registration_screen.dart
+++ flash_chat/lib/screens/registration_screen.dart
@@ -18,96 +18,101 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   String? userEmail;
   String? password;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: SizedBox(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
-                ),
-              ),
-              const SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (value) {
-                    userEmail = value;
-                  },
-                  decoration: textFieldInputDecoration.copyWith(
-                    hintText: 'Enter your email',
-                  )),
-              const SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                obscureText: true,
-                keyboardType: TextInputType.visiblePassword,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: textFieldInputDecoration.copyWith(
-                  hintText: 'Enter your password',
-                ),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              RoundedButton(
-                buttonText: 'Register',
-                color: Colors.blueAccent,
-                onPressed: () async {
-                  FocusManager.instance.primaryFocus!.unfocus();
-                  if (userEmail == null && password == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('E-mail Id and Password can\'t be empty!!'),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Hero(
+                      tag: 'logo',
+                      child: SizedBox(
+                        height: 200.0,
+                        child: Image.asset('images/logo.png'),
                       ),
-                    );
-                  } else {
-                    final message = await AuthService.registration(email: userEmail!, password: password!);
-                    if (message.contains('Success')) {
-                      Navigator.pushNamed(context, LoginScreen.id);
-                    } else if (userEmail == null || password == null) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('E-mail Id or Password can\'t be empty!!'),
-                        ),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(message),
-                        ),
-                      );
-                    }
-                  }
-                },
+                    ),
+                    const SizedBox(
+                      height: 48.0,
+                    ),
+                    TextField(
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) {
+                          userEmail = value;
+                        },
+                        decoration: textFieldInputDecoration.copyWith(
+                          hintText: 'Enter your email',
+                        )),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    TextField(
+                      textAlign: TextAlign.center,
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      decoration: textFieldInputDecoration.copyWith(
+                        hintText: 'Enter your password',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    RoundedButton(
+                      buttonText: 'Register',
+                      color: Colors.blueAccent,
+                      onPressed: () async {
+                        FocusManager.instance.primaryFocus!.unfocus();
+
+                        if (userEmail == null || password == null) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('E-mail Id and Password can\'t be empty!!'),
+                            ),
+                          );
+                        } else {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          final message = await AuthService.registration(email: userEmail!, password: password!);
+                          setState(() {
+                            isLoading = false;
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(message),
+                            ),
+                          );
+                          if (message.contains('Success')) {
+                            Navigator.pushNamed(context, LoginScreen.id);
+                          }
+                        }
+                      },
+                    ),
+                    RoundedButton(
+                      buttonText: 'Log in',
+                      color: Colors.grey.shade400,
+                      onPressed: () async {
+                        Navigator.pushNamed(context, LoginScreen.id);
+                      },
+                    ),
+                  ],
+                ),
               ),
-              RoundedButton(
-                buttonText: 'Log in',
-                color: Colors.grey.shade400,
-                onPressed: () async {
-                  Navigator.pushNamed(context, LoginScreen.id);
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
